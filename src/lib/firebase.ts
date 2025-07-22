25f45771f74e9a41e81e5fea77b69d56c0d5a6e4
COOL BOY DJ RATHAN <coolboydjrathan@gmail.com>
2025-07-22 12:18:49 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,15 @@
-// @ts-nocheck
+
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  "projectId": "proflow-8dbu8",
-  "appId": "1:1091005716189:web:8c4f5e17298714fc31ed34",
-  "storageBucket": "proflow-8dbu8.firebasestorage.app",
-  "apiKey": "AIzaSyBqhCw9LHugRvO9_qSXgn3B7_nwS6K-s-Q",
-  "authDomain": "proflow-8dbu8.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1091005716189"
+  apiKey: "AIzaSyBqhCw9LHugRvO9_qSXgn3B7_nwS6K-s-Q",
+  authDomain: "proflow-8dbu8.firebaseapp.com",
+  projectId: "proflow-8dbu8",
+  storageBucket: "proflow-8dbu8.firebasestorage.app",
+  messagingSenderId: "1091005716189",
+  appId: "1:1091005716189:web:8c4f5e17298714fc31ed34"
 };
 
 // Initialize Firebase
@@ -18,4 +17,4 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-export { db, auth };
+export { app, db, auth };
