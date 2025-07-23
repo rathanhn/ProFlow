5bb7858f3784e018db65214f0b2bca0d789f47e4
COOL BOY DJ RATHAN <coolboydjrathan@gmail.com>
2025-07-23 14:22:29 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -29,7 +29,7 @@ export const SidebarProvider = ({ children }: { children: React.ReactNode }) => 
   )
 }
 
-const useSidebar = () => {
+export const useSidebar = () => {
   const context = React.useContext(SidebarContext)
   if (!context) {
     throw new Error("useSidebar must be used within a SidebarProvider")
