40f1c09e32c8958b22c55dd5491feed72fe3e361
COOL BOY DJ RATHAN <coolboydjrathan@gmail.com>
2025-07-25 19:08:30 +0000

    Same issue Looks like half content is cropped (right content is cropped)

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -197,7 +197,7 @@ const DashboardContent = ({
   
 
   return (
-    <div className="relative flex min-h-screen">
+    <div className="flex min-h-screen">
       <Sidebar>
         <SidebarHeader>
           <Rocket className="w-6 h-6 text-primary" />
@@ -343,12 +343,11 @@ const DashboardContent = ({
           <UserProfile />
         </SidebarFooter>
       </Sidebar>
-      <div className="flex-1 flex flex-col">
-        <header className={cn(
-          "flex items-center justify-between p-4 border-b h-16 sticky top-0 bg-background z-30 transition-all duration-300 ease-in-out",
-          "lg:ml-16",
-          !isCollapsed && "lg:ml-64"
-        )}>
+      <div className={cn(
+        "flex-1 flex flex-col transition-all duration-300 ease-in-out",
+        !isCollapsed ? "lg:ml-64" : "lg:ml-16"
+      )}>
+        <header className="flex items-center justify-between p-4 border-b h-16 sticky top-0 bg-background z-30">
             <div className="flex items-center">
               <SidebarTrigger />
             </div>
@@ -356,11 +355,7 @@ const DashboardContent = ({
               {user && <NotificationBell />}
             </div>
         </header>
-        <main className={cn(
-            "flex-1 p-4 sm:p-6 lg:p-8 bg-muted/40 transition-all duration-300 ease-in-out",
-            "lg:ml-16",
-            !isCollapsed && "lg:ml-64"
-        )}>
+        <main className="flex-1 p-4 sm:p-6 lg:p-8 bg-muted/40">
           <div className="mx-auto max-w-7xl">
             {children}
           </div>
