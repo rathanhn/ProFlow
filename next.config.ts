f58561242079643a73b2f93268936b208ebd216c
COOL BOY DJ RATHAN <coolboydjrathan@gmail.com>
2025-07-25 20:44:46 +0000

    Is everything set to Deploy ?

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
